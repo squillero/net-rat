cb730729f87f3a57133357bab8719eae21afaa79
Giovanni Squillero <squillero@polito.it>
2024-03-29 18:39:02 +0100

    Update LICENSE

--- channels.go
+++ channels.go
@@ -3,11 +3,7 @@
 // `~~~~~\_;m__m._>o   Yet another little Go experiment
 //
 // Copyright © 2024 Giovanni Squillero <giovanni.squillero@polito.it>
-// This code is being released for EDUCATIONAL and ACADEMIC purposes.
-// +----------------------------------------------------------------+
-// | <=*=> COMMERCIAL USE OF THE CODE IS EXPRESSLY PROHIBITED <=*=> |
-// +----------------------------------------------------------------+
-// The code is made available "AS-IS" without any express or implied
-// guarantees such as fitness, merchantability, or non-infringement.
+// This code is being released for Educational and Academic purposes.
+// Commercial use is expressly prohibited (see LICENCE for details).
 
 package main
